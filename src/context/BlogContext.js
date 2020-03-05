ce13c85f40bbc186bcd671f81acd94f209c0b496
konghiran_won <konghiran.won@ascendcorp.com>
2020-03-05 20:44:14 +0700

    replace useState with useReducer

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,14 +1,25 @@
-import React, {useState} from 'react';
+import React, { useReducer } from 'react';
 
 const BlogContext  = React.createContext();
+
+const blogReducer = (state,action) => {
+        switch(action.type){
+            case 'add_blogpost':
+                return [...state, { title : `blog post#${state.length + 1}`}];
+            default :
+                return state;
+        }
+
+
+}
 
 export const BlogProvider = ({ children }) => { 
     // <View> < this is props.children /> <View>
     // this is not default export so use { } when import
-    const [blogPosts,setBlogPosts] = useState([]);
+    const [blogPosts,dispatch] = useReducer(blogReducer,[]);
 
     const addBlogPost = () => {
-        setBlogPosts([...blogPosts,{ title: `blog post #${blogPosts.length + 1}`} ]); // addBlogPosts === old array + new object 
+        dispatch( { type:'add_blogpost' } );
     }
 
     return <BlogContext.Provider value={{ data: blogPosts, addBlogPost:addBlogPost }}>{children}</BlogContext.Provider>
