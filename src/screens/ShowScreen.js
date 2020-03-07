1402dae21660efc18c624d2e8bf258bc1573b62c
konghiran_won <konghiran.won@ascendcorp.com>
2020-03-07 13:32:12 +0700

    add edit button on show screen

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,6 +1,7 @@
-import React, { useContext} from 'react';
-import {View , Text , StyleSheet } from 'react-native';
+import React, { useContext } from 'react';
+import { View , Text , StyleSheet, TouchableOpacity } from 'react-native';
 import { Context } from '../context/BlogContext';
+import { Feather } from '@expo/vector-icons';
 
 const ShowScreen = ({navigation}) => {
     
@@ -14,8 +15,21 @@ const ShowScreen = ({navigation}) => {
             <Text>{blogPost.content}</Text>
         </View>
     );
-    
+
 }
+
+ShowScreen.navigationOptions = ({navigation}) => {
+    return { 
+        headerRight: (
+        <TouchableOpacity
+            onPress={()=>navigation.navigate('Edit')}
+        >
+        <Feather name='edit' size={30}/>
+        </TouchableOpacity>
+        )
+        };
+}
+
 
 const styles = StyleSheet.create({});
 
